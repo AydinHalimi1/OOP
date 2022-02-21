d5a4e6d6b60a66eaa71f2755977d28f1534d0a12
Aydin Halimi <aydin_halimi1@baylor.edu>
2022-02-21 15:45:44 -0600

    CellPhone and Student

---
+++ StudentClass.py
@@ -0,0 +1,9 @@
+class student:
+    def __init__(self,ID, FullName, birthday, year):
+        self.StudentID = ID
+        self.Name = FullName
+        self.DOB = birthday
+        self.classification = year
+
+    def set_age
+    def set_registration_date
